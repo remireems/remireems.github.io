c041139d84f573238e6f28c24ed7428aa545ba03
Remi <75871744+remireems@users.noreply.github.com>
2022-02-03 16:10:30 -0800

    added font style

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
+import About from './components/About'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import Intro from './components/Intro/Intro'
+import Navbar from './components/Navbar/Navbar'
+import Projects from './components/Projects/Projects'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-  
+      <Navbar />
+      <Intro />
+      <About />
+      <Projects />
+      <Contact />
+      <Footer />
     </div>
   )
 }
