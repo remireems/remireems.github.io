f69057c54c79bfb95d7619e8863bd71ecc984f59
Remi <75871744+remireems@users.noreply.github.com>
2022-02-03 17:45:03 -0800

    added icons to intro

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './Intro.css'
 
 const Intro = () => {
@@ -13,6 +15,11 @@ const Intro = () => {
             <div className="title">Finance Background</div>
             <div className="title">Baker</div>
           </div>
+        </div>
+        <div className="iAcc">
+          <FontAwesomeIcon icon={faGithub} size='lg' />
+          <FontAwesomeIcon icon={faLinkedin} size='lg' />
+          <FontAwesomeIcon icon={faInstagram} size='lg' /><FontAwesomeIcon icon={faFacebook} size='lg' />
         </div>
       </div>
     </div>
