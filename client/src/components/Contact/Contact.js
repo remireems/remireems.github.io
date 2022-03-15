20c7931a5b683d901c570ec77a8c0ebbab007273
Remi <75871744+remireems@users.noreply.github.com>
2022-03-14 17:12:29 -0700

    added package and code

--- client/src/components/Contact/Contact.js
+++ client/src/components/Contact/Contact.js
@@ -1,14 +1,7 @@
-import { useState } from 'react'
-import { Button, Modal } from 'react-bootstrap'
 import ContactForm from '../ContactForm/ContactForm'
 import './Contact.css'
 
-const Contact = (props) => {
-
-  const [show, setShow] = useState(false);
-
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+const Contact = () => {
 
   return (
     <div className="conSec">
@@ -18,28 +11,9 @@ const Contact = (props) => {
           <p className="conText">I am looking for opportunities to learn, gain experiences, and  improve my skills! Please reach out to me, I would love to get in touch with you. </p>
         </div>
         <div>
-          <Button variant="primary" onClick={handleShow}>
-            Get in Contact
-          </Button>
-
-          <Modal {...props} show={show} onHide={handleClose}
-            aria-labelledby="contained-modal-title-vcenter"
-            centered>
-            <Modal.Header closeButton>
-              <Modal.Title>Get in contact by sending me an email!</Modal.Title>
-            </Modal.Header>
-            <Modal.Body><ContactForm /></Modal.Body>
-            <Modal.Footer>
-              <Button variant="secondary" onClick={handleClose}>
-                Close
-              </Button>
-              <Button variant="primary" onClick={handleClose}>
-                Send
-              </Button>
-            </Modal.Footer>
-          </Modal>
+          <ContactForm />
         </div>
-
+        
       </div>
     </div>
   )
--- client/src/components/ContactForm/ContactForm.js
+++ client/src/components/ContactForm/ContactForm.js
@@ -1,22 +1,69 @@
-import { Form, Row } from 'react-bootstrap'
+import { useRef, useState } from 'react'
+import { Modal, Form, Row, Button } from 'react-bootstrap'
+import emailjs from '@emailjs/browser'
 
-const ContactForm = () => {
+const ContactForm = (props) => {
+
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const formRef = useRef()
+
+  const handleSend = event => {
+    event.preventDefault()
+
+    emailjs.sendForm('service_loewtu5', 'template_jkih7i9', 
+      formRef.current, '9yab1r4ZWVrO1PBhw')
+      .then((result) => {
+        handleClose()
+        alert('Email sent!')
+        console.log(result.text);      
+      }, (error) => {
+        console.log(error.text);
+      });
+  }
+
   return (
     <div>
-      <Form>
-        <Row className="mb-3">
-          <Form.Control placeholder="Name" />
-        </Row>
-        <Row className="mb-3">
-          <Form.Control placeholder="Email" />
-        </Row>
-        <Row className="mb-3">
-          <Form.Control placeholder="Subject" />
-        </Row>
-        <Row className="mb-2">
-          <Form.Control as="textarea" placeholder="Enter message" rows={3} />
-        </Row>
-      </Form>
+      <Button variant="primary" onClick={handleShow}>
+        Get in Contact
+      </Button>
+
+      <Modal {...props} show={show} onHide={handleClose}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Get in contact by sending me an email!</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form ref={formRef} >
+            <Row className="mb-3">
+              <Form.Control type="text" placeholder="Name" name="userName" />
+            </Row>
+            <Row className="mb-3">
+              <Form.Control type="text" placeholder="Email" name="userEmail" />
+            </Row>
+            <Row className="mb-3">
+              <Form.Control type="text" placeholder="Subject" name="userSubject" />
+            </Row>
+            <Row className="mb-3">
+              <Form.Control type="text" as="textarea" placeholder="Enter message" rows={3} name="userMessage" />
+            </Row>
+            <div className='contactBtns'>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button 
+              className='ms-2'
+              variant="primary" onClick={handleSend}>
+                Send
+              </Button>
+            </div>
+          </Form>
+        </Modal.Body>
+      </Modal>
     </div>
   )
 }
