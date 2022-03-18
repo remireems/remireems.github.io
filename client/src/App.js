766ea6cc3fc42908f3853bfe60e11e228fb8c066
Remi <75871744+remireems@users.noreply.github.com>
2022-03-18 16:04:25 -0700

    changes in npm & added import in appjs

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Intro from './components/Intro/Intro'
 import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
 // import { library } from '@fortawesome/fontawesome-svg-core'
 // import { fab } from '@fortawesome/free-brands-svg-icons'
 import './App.css'
