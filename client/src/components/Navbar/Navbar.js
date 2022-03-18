682f7dfa9af0d9dd7a4f33fd84c8a076a3b45f84
Remi <75871744+remireems@users.noreply.github.com>
2022-03-17 17:56:19 -0700

    code

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 // import { Navbar, Container, Nav, Button } from 'react-bootstrap'
 import { useState } from 'react';
-import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem } from '@mui/material'
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem, Tabs, Tab } from '@mui/material'
+import {MenuIcon } from '@mui/icons-material';
 
 import './Navbar.css'
 
-const pages = ['About', 'Projects', 'Contact'];
+const pages = ['About', 'Projects', 'Contact', 'Resume'];
 
 const Navbar = () => {
 
@@ -28,98 +28,137 @@ const Navbar = () => {
   //   </div>
   // )
 
-  const [anchorElNav, setAnchorElNav] = useState(null);
-  const [anchorElUser, setAnchorElUser] = useState(null);
+  // const [anchorElNav, setAnchorElNav] = useState(null);
+  // const [anchorElUser, setAnchorElUser] = useState(null);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.currentTarget);
-  };
+  // const handleOpenNavMenu = (event) => {
+  //   setAnchorElNav(event.currentTarget);
+  // };
+  // const handleOpenUserMenu = (event) => {
+  //   setAnchorElUser(event.currentTarget);
+  // };
 
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  // const handleCloseNavMenu = () => {
+  //   setAnchorElNav(null);
+  // };
 
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
+  // const handleCloseUserMenu = () => {
+  //   setAnchorElUser(null);
+  // };
 
-  return (
-    <AppBar position="static">
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-          >
-            LOGO
-          </Typography>
+  // return (
+  //   <AppBar position="fixed">
+  //     <Container maxWidth="xl">
+  //       <Toolbar disableGutters>
+  //         <Typography
+  //           variant="h6"
+  //           noWrap
+  //           component="div"
+  //           sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+  //         >
+  //           LOGO
+  //         </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-              {pages.map((page) => (
-                <MenuItem key={page} onClick={handleCloseNavMenu}>
-                  <Typography textAlign="center">{page}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-          >
-            LOGO
-          </Typography>
-          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-            {pages.map((page) => (
-              <Button
-                key={page}
-                onClick={handleCloseNavMenu}
-                sx={{ my: 2, color: 'white', display: 'block' }}
-              >
-                {page}
-              </Button>
-            ))}
-          </Box>
+  //         <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+  //           <IconButton
+  //             size="large"
+  //             aria-label="account of current user"
+  //             aria-controls="menu-appbar"
+  //             aria-haspopup="true"
+  //             onClick={handleOpenNavMenu}
+  //             color="inherit"
+  //           >
+  //             <MenuIcon />
+  //           </IconButton>
+  //           <Menu
+  //             id="menu-appbar"
+  //             anchorEl={anchorElNav}
+  //             anchorOrigin={{
+  //               vertical: 'bottom',
+  //               horizontal: 'left',
+  //             }}
+  //             keepMounted
+  //             transformOrigin={{
+  //               vertical: 'top',
+  //               horizontal: 'left',
+  //             }}
+  //             open={Boolean(anchorElNav)}
+  //             onClose={handleCloseNavMenu}
+  //             sx={{
+  //               display: { xs: 'block', md: 'none' },
+  //             }}
+  //           >
+  //             {pages.map((page) => (
+  //               <MenuItem key={page} onClick={handleCloseNavMenu}>
+  //                 <Typography textAlign="center">{page}</Typography>
+  //               </MenuItem>
+  //             ))}
+  //           </Menu>
+  //         </Box>
+  //         <Typography
+  //           variant="h6"
+  //           noWrap
+  //           component="div"
+  //           sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+  //         >
+  //           LOGO
+  //         </Typography>
+  //         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+  //           {pages.map((page) => (
+  //             <Button
+  //               key={page}
+  //               onClick={handleCloseNavMenu}
+  //               sx={{ my: 2, color: 'white', display: 'block' }}
+  //             >
+  //               {page}
+  //             </Button>
+  //           ))}
+  //         </Box>
 
           
-        </Toolbar>
+  //       </Toolbar>
+  //     </Container>
+  //   </AppBar>
+  // );
+
+
+
+  function LinkTab(props) {
+    return (
+      <Tab
+        component="a"
+        onClick={(event) => {
+          event.preventDefault();
+        }}
+        {...props}
+      />
+    );
+  }
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+
+  return (
+    <AppBar position='fixed'>
+      <Container>
+        <Box>
+          Home
+        </Box>
+        <Box>
+          <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+            <LinkTab label="Page One" href="/drafts" />
+            <LinkTab label="Page Two" href="/trash" />
+            <LinkTab label="Page Three" href="/spam" />
+          </Tabs>
+        </Box>
       </Container>
     </AppBar>
-  );
+
+  )
 
 }
 
