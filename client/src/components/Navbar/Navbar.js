1ab36c2bd64ecba9d68285657cece15e0f8985b7
Remi <75871744+remireems@users.noreply.github.com>
2022-03-18 16:32:35 -0700

    code

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,165 +1,32 @@
-// import { Navbar, Container, Nav, Button } from 'react-bootstrap'
-import { useState } from 'react';
-import { AppBar, Box, Toolbar, IconButton, Typography, Menu, Container, Avatar, Button, Tooltip, MenuItem, Tabs, Tab } from '@mui/material'
-import {MenuIcon } from '@mui/icons-material';
+import { Navbar, Container, Nav, Button } from 'react-bootstrap'
+import { useState } from 'react'
+
 
 import './Navbar.css'
 
-const pages = ['About', 'Projects', 'Contact', 'Resume'];
-
-const Navbar = () => {
-
-  // return (
-  //   <div className="navSec">
-  //     <Navbar bg="light" expand="lg" id='navStyle'>
-  //       <Container id='navCont'>
-  //         <Navbar.Brand href="/Intro">RK</Navbar.Brand>
-  //         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  //         <Navbar.Collapse id="basic-navbar-nav">
-  //           <Nav className="me-auto">
-  //             <Nav.Link href="/About">About</Nav.Link>
-  //             <Nav.Link href="/Projects">Projects</Nav.Link>
-  //             <Nav.Link href="/Contact">Contact</Nav.Link>
-  //             <Button>Resume</Button>              
-  //           </Nav>
-  //         </Navbar.Collapse>
-  //       </Container>
-  //     </Navbar>
-  //   </div>
-  // )
-
-  // const [anchorElNav, setAnchorElNav] = useState(null);
-  // const [anchorElUser, setAnchorElUser] = useState(null);
-
-  // const handleOpenNavMenu = (event) => {
-  //   setAnchorElNav(event.currentTarget);
-  // };
-  // const handleOpenUserMenu = (event) => {
-  //   setAnchorElUser(event.currentTarget);
-  // };
-
-  // const handleCloseNavMenu = () => {
-  //   setAnchorElNav(null);
-  // };
-
-  // const handleCloseUserMenu = () => {
-  //   setAnchorElUser(null);
-  // };
-
-  // return (
-  //   <AppBar position="fixed">
-  //     <Container maxWidth="xl">
-  //       <Toolbar disableGutters>
-  //         <Typography
-  //           variant="h6"
-  //           noWrap
-  //           component="div"
-  //           sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-  //         >
-  //           LOGO
-  //         </Typography>
-
-  //         <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-  //           <IconButton
-  //             size="large"
-  //             aria-label="account of current user"
-  //             aria-controls="menu-appbar"
-  //             aria-haspopup="true"
-  //             onClick={handleOpenNavMenu}
-  //             color="inherit"
-  //           >
-  //             <MenuIcon />
-  //           </IconButton>
-  //           <Menu
-  //             id="menu-appbar"
-  //             anchorEl={anchorElNav}
-  //             anchorOrigin={{
-  //               vertical: 'bottom',
-  //               horizontal: 'left',
-  //             }}
-  //             keepMounted
-  //             transformOrigin={{
-  //               vertical: 'top',
-  //               horizontal: 'left',
-  //             }}
-  //             open={Boolean(anchorElNav)}
-  //             onClose={handleCloseNavMenu}
-  //             sx={{
-  //               display: { xs: 'block', md: 'none' },
-  //             }}
-  //           >
-  //             {pages.map((page) => (
-  //               <MenuItem key={page} onClick={handleCloseNavMenu}>
-  //                 <Typography textAlign="center">{page}</Typography>
-  //               </MenuItem>
-  //             ))}
-  //           </Menu>
-  //         </Box>
-  //         <Typography
-  //           variant="h6"
-  //           noWrap
-  //           component="div"
-  //           sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-  //         >
-  //           LOGO
-  //         </Typography>
-  //         <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-  //           {pages.map((page) => (
-  //             <Button
-  //               key={page}
-  //               onClick={handleCloseNavMenu}
-  //               sx={{ my: 2, color: 'white', display: 'block' }}
-  //             >
-  //               {page}
-  //             </Button>
-  //           ))}
-  //         </Box>
-
-          
-  //       </Toolbar>
-  //     </Container>
-  //   </AppBar>
-  // );
 
 
-
-  function LinkTab(props) {
-    return (
-      <Tab
-        component="a"
-        onClick={(event) => {
-          event.preventDefault();
-        }}
-        {...props}
-      />
-    );
-  }
-
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
+const NavMenu = () => {
 
   return (
-    <AppBar position='fixed'>
-      <Container>
-        <Box>
-          Home
-        </Box>
-        <Box>
-          <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
-            <LinkTab label="Page One" href="/drafts" />
-            <LinkTab label="Page Two" href="/trash" />
-            <LinkTab label="Page Three" href="/spam" />
-          </Tabs>
-        </Box>
-      </Container>
-    </AppBar>
-
-  )
+    <div className="navSec">
+      <Navbar bg="light" expand="lg" id='navStyle'>
+        <Container id='navCont'>
+          <Navbar.Brand href="/Intro">RK</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="/About">About</Nav.Link>
+              <Nav.Link href="/Projects">Projects</Nav.Link>
+              <Nav.Link href="/Contact">Contact</Nav.Link>
+              <Button>Resume</Button>              
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
 
 }
 
-export default Navbar
+export default NavMenu
--- client/src/components/Project/Project.js
+++ client/src/components/Project/Project.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
-// import { Card } from 'react-bootstrap'
+import { Card, Row, Col } from 'react-bootstrap'
 import './Project.css'
 
 const Project = ({ title, des, techs, link, ghLink }) => {
@@ -30,19 +30,38 @@ const Project = ({ title, des, techs, link, ghLink }) => {
     //   </ul>
     // </div>
     <div>
-      {/* <Card style={{ width: '18rem' }}>
+      <Card style={{ width: '18rem' }}>
         <Card.Body>
           <Card.Title>{title}</Card.Title>
           <Card.Text>{des}</Card.Text>
           <ul className="pTechLi">
             {techList}
-            
+
           </ul>          
           <Card.Link href={link} target="_blank" ><FontAwesomeIcon icon={faLink} size='lg' /></Card.Link>
           <Card.Link href={ghLink} target="_blank"><FontAwesomeIcon icon={faGithub} size='lg' /></Card.Link>
         </Card.Body>
-      </Card> */}
+      </Card>
     </div>
+
+    // <Row xs={1} md={2} className="g-4">
+    //   {Array.from({ length: 1 }).map((_, idx) => (
+    //     <Col>
+    //       <Card style={{ width: '18rem' }}>
+    //         <Card.Body>
+    //           <Card.Title>{title}</Card.Title>
+    //           <Card.Text>{des}</Card.Text>
+    //           <ul className="pTechLi">
+    //             {techList}
+
+    //           </ul>
+    //           <Card.Link href={link} target="_blank" ><FontAwesomeIcon icon={faLink} size='lg' /></Card.Link>
+    //           <Card.Link href={ghLink} target="_blank"><FontAwesomeIcon icon={faGithub} size='lg' /></Card.Link>
+    //         </Card.Body>
+    //       </Card>
+    //     </Col>
+    //   ))}
+    // </Row>
   )
 }
 
