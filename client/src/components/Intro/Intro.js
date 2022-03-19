7369614c298861118bdd20c570c70f0be6c1eb4a
Remi <75871744+remireems@users.noreply.github.com>
2022-03-18 17:44:51 -0700

    add&style links to icons

--- client/src/components/Intro/Intro.js
+++ client/src/components/Intro/Intro.js
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFacebook, faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faGithub, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 import './Intro.css'
 
 const Intro = () => {
@@ -8,7 +8,6 @@ const Intro = () => {
       <div className="introDiv">
         <h2>Hello, my name is</h2>
         <h1 className="iName">Remi Kim.</h1>
-        {/* <h2>I'm a full-stack web developer with a baking and finance background.</h2> */}
         <div className="titlesDiv">
           <div className="titlesBox">
             <div className="title">Web Developer</div>
@@ -16,10 +15,10 @@ const Intro = () => {
             <div className="title">Baker</div>
           </div>
         </div>
-        <div className="iAcc">
-          <FontAwesomeIcon icon={faGithub} size='lg' />
-          <FontAwesomeIcon icon={faLinkedin} size='lg' />
-          <FontAwesomeIcon icon={faInstagram} size='lg' /><FontAwesomeIcon icon={faFacebook} size='lg' />
+        <div className="iAcc mt-2">
+          <a href="https://github.com/remireems" target='_blank' rel="noreferrer" className='iIcons'><FontAwesomeIcon className="me-3" icon={faGithub} size='2x' /></a>
+          <a href="https://www.linkedin.com/in/remi-kim-186113217/" target='_blank' rel="noreferrer" className='iIcons'><FontAwesomeIcon className="me-3" icon={faLinkedin} size='2x' /></a>
+          <a href="https://www.instagram.com/heeeewonkim/?hl=en" target='_blank' rel="noreferrer" className='iIcons'><FontAwesomeIcon className="me-3" icon={faInstagram} size='2x' /></a>      
         </div>
       </div>
     </div>
