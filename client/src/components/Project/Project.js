c85dd581889855c4842a7db2cde75d5da0a2fcc8
Remi <75871744+remireems@users.noreply.github.com>
2022-03-18 17:12:51 -0700

    cleaned up code

--- client/src/components/Project/Project.js
+++ client/src/components/Project/Project.js
@@ -1,7 +1,7 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faLink } from '@fortawesome/free-solid-svg-icons'
-import { Card, Row, Col } from 'react-bootstrap'
+import { Card } from 'react-bootstrap'
 import './Project.css'
 
 const Project = ({ title, des, techs, link, ghLink }) => {
@@ -11,57 +11,17 @@ const Project = ({ title, des, techs, link, ghLink }) => {
   )
 
   return (
-    // <div className="pCard">
-    //   <div className="pIcon">
-    //     <a href={ghLink} target="_blank" rel="noreferrer" className="ghIcon">
-    //       <FontAwesomeIcon icon={faGithub} size='lg'/>
-    //     </a>
-    //   </div>
-    //   <h2>
-    //     <a href={link} target="_blank" rel="noreferrer" className="ghIcon">
-    //       {title}
-    //     </a>
-    //   </h2>
-    //   <div className="pDes">
-    //     <p>{des}</p>
-    //   </div>
-    //   <ul className="pTechLi">
-    //     {techList}
-    //   </ul>
-    // </div>
-    // <div>
-      <Card className="mb-3" style={{ width: '18rem' }}>
-        <Card.Body>
-          <Card.Title>{title}</Card.Title>
-          <Card.Text>{des}</Card.Text>
-          <ul className="pTechLi">
-            {techList}
-
-          </ul>          
-          <Card.Link href={link} target="_blank" ><FontAwesomeIcon icon={faLink} size='lg' /></Card.Link>
-          <Card.Link href={ghLink} target="_blank"><FontAwesomeIcon icon={faGithub} size='lg' /></Card.Link>
-        </Card.Body>
-      </Card>
-    //  </div>
-
-    // <Row xs={1} md={2} className="g-4">
-    //   {Array.from({ length: 1 }).map((_, idx) => (
-    //     <Col>
-    //       <Card style={{ width: '18rem' }}>
-    //         <Card.Body>
-    //           <Card.Title>{title}</Card.Title>
-    //           <Card.Text>{des}</Card.Text>
-    //           <ul className="pTechLi">
-    //             {techList}
-
-    //           </ul>
-    //           <Card.Link href={link} target="_blank" ><FontAwesomeIcon icon={faLink} size='lg' /></Card.Link>
-    //           <Card.Link href={ghLink} target="_blank"><FontAwesomeIcon icon={faGithub} size='lg' /></Card.Link>
-    //         </Card.Body>
-    //       </Card>
-    //     </Col>
-    //   ))}
-    // </Row>
+    <Card className="mb-3" style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>{des}</Card.Text>
+        <ul className="pTechLi">
+          {techList}
+        </ul>
+        <Card.Link href={link} target="_blank" ><FontAwesomeIcon icon={faLink} size='lg' /></Card.Link>
+        <Card.Link href={ghLink} target="_blank"><FontAwesomeIcon icon={faGithub} size='lg' /></Card.Link>
+      </Card.Body>
+    </Card>
   )
 }
 
