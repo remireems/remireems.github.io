88c2899926b80b10ec6e7e749170491dc53a3e9b
Remi <75871744+remireems@users.noreply.github.com>
2022-04-18 15:14:06 -0700

    clean up code

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,6 @@ import Navbar from './components/Navbar/Navbar'
 import Projects from './components/Projects/Projects'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import { library } from '@fortawesome/fontawesome-svg-core'
-// import { fab } from '@fortawesome/free-brands-svg-icons'
 import './App.css'
 
 function App() {
