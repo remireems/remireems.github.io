ee96199b05cda7c453ccc53c07a22cb91ad1d5c2
Remi <75871744+remireems@users.noreply.github.com>
2022-04-25 16:07:57 -0700

    updated about me info

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,9 +7,9 @@ const About = () => {
     <Container id='about'>
       <div className="aboutDes">
         <h1>About Me</h1>
-        <p>Hello, my name is Remi Kim. Web development caught my interest in 2021. I was able to teach myself some basic code on building a website with the help of free online courses. It is  satisfying to see what I coded and created with my own effort. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque sapiente deserunt quis temporibus expedita, illo corrupti quae soluta laborum ipsam culpa maiores error numquam deleniti dolorum doloribus nesciunt. Possimus, et!</p>
+        <p>Hello, my name is Remi Kim. I am a full stack web developer with a finance background and a certificate in full stack development from UC Irvine Extended Education. Skills in JavaScript, Node.js, MySQL, MongoDB, and React.js. Detailed-oriented and passionate about developing applications to create better user web experiences. With each project, my goal is to ensure an impactful user experience and build apps that are an improvement of my prior projects. In my lastest project, I was in a team of six developing a fun and educational app that simulates cryptocurrency trading with fake money. It is a gateway for people who are new to crypto and are interested in coin trading. My role in the project was to create,style, and make the webpage mobile-friendly as a front-end developer. Always striving to create better user experiences for a company and users.</p>
         <p>My Skills:</p>
-        <p>HTML, CSS, JavaScript, React</p>        
+        <p>HTML, CSS, JavaScript, React, Node.js, MySQL, MongoDB</p>        
       </div>
       <div className="aboutImg">
         <div className="aPicDiv">
