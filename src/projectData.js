f2027b0c11b5267df1f61aac16e897e1073987f6
Remi <75871744+remireems@users.noreply.github.com>
2022-05-19 16:11:29 -0700

    updated project data

--- src/projectData.js
+++ src/projectData.js
@@ -1,14 +1,6 @@
 export const prosData = [
   {
     id: 1,
-    title: 'My Portfolio',
-    description: 'My web developer portfolio',
-    techs: ['React', 'JavaScript', 'Node.js'],
-    link: 'https://remikim.herokuapp.com/',
-    ghLink: 'https://github.com/remireems/remireems.github.io'
-  },
-  {
-    id: 2,
     title: 'Food For You',
     description: 'For my first group project, I worked as a front-end developer. Our team developed a food application that will help you plan a meal by entering in the ingredients you have at home!',
     techs: ['HTML', 'CSS', 'JavaScript'],
@@ -16,7 +8,7 @@ export const prosData = [
     ghLink: 'https://github.com/remireems/FoodForYou'
   },
   {
-    id: 3,
+    id: 2,
     title: 'PUB',
     description: 'Another group project as a front-end developer. Our team developed an application where you can create, share, and track projects.',
     techs: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
@@ -24,11 +16,19 @@ export const prosData = [
     ghLink: 'https://github.com/sequint/pub'
   },
   {
-    id: 4,
+    id: 3,
     title: 'CryptoMaster',
     description: 'Another group project as a front-end developer. Our team developed a crypto-trading stimulator as a fun learning game for people who are interested in crypto.',
     techs: ['React', 'Node.js', 'MongoDB'],
     link: 'https://vast-headland-48199.herokuapp.com/signin',
     ghLink: 'https://github.com/Lan714/CryptoMaster'
+  },
+  {
+    id: 4,
+    title: 'Weather App',
+    description: 'Simple weather app project made for fun and practice. You can search the current weather of any city. The app displays the city, current date, weather, and a weather image. This app is mainly used for small screens.',
+    techs: ['React', 'JavaScript', 'Axios', 'Weather API'],
+    link: 'https://young-woodland-57453.herokuapp.com/',
+    ghLink: 'https://github.com/remireems/weatherApp'
   }
 ]
