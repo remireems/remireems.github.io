fc63147561904900261a3afb8ee9af32dc1f9fce
Remi <75871744+remireems@users.noreply.github.com>
2022-06-03 16:10:30 -0700

    added project

--- src/projectData.js
+++ src/projectData.js
@@ -2,7 +2,7 @@ export const prosData = [
   {
     id: 1,
     title: 'Food For You',
-    description: 'For my first group project, I worked as a front-end developer. Our team developed a food application that will help you plan a meal by entering in the ingredients you have at home!',
+    description: 'For my first group project, I worked as a front-end dev. Our team developed a food application that will help you plan a meal by entering in the ingredients you have at home!',
     techs: ['HTML', 'CSS', 'JavaScript'],
     link: 'https://gresendi.github.io/FoodForYou/',
     ghLink: 'https://github.com/remireems/FoodForYou'
@@ -10,7 +10,7 @@ export const prosData = [
   {
     id: 2,
     title: 'PUB',
-    description: 'Another group project as a front-end developer. Our team developed an application where you can create, share, and track projects.',
+    description: 'Another group project as a front-end dev. Our team developed an application where you can create, share, and track projects.',
     techs: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
     link: 'https://pure-brook-86019.herokuapp.com/',
     ghLink: 'https://github.com/sequint/pub'
@@ -18,17 +18,17 @@ export const prosData = [
   {
     id: 3,
     title: 'CryptoMaster',
-    description: 'Another group project as a front-end developer. Our team developed a crypto-trading stimulator as a fun learning game for people who are interested in crypto.',
+    description: 'Another group project as a front-end dev. Our team developed a crypto-trading stimulator as a fun learning game for people who are interested in crypto.',
     techs: ['React', 'Node.js', 'MongoDB'],
     link: 'https://vast-headland-48199.herokuapp.com/signin',
     ghLink: 'https://github.com/Lan714/CryptoMaster'
   },
   {
     id: 4,
-    title: 'Weather App',
-    description: 'Simple weather app project made for fun and practice. You can search the current weather of any city. The app displays the city, current date, weather, and a weather image. This app is mainly used for small screens.',
-    techs: ['React', 'JavaScript', 'Axios', 'Weather API'],
-    link: 'https://young-woodland-57453.herokuapp.com/',
-    ghLink: 'https://github.com/remireems/weatherApp'
+    title: 'fakeAmazon',
+    description: 'E-commerce project as a full-stack dev. Tried to clone the Amazon website, users can create an account and sign in to purchase products.',
+    techs: ['React', 'Stripe', 'Firebase'],
+    link: 'https://clone-bc0bd.web.app/',
+    ghLink: 'https://github.com/remireems/amazonClone'
   }
 ]
